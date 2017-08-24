8b2cd9c853bcd95d086c773e114ae5d673c23ddc
Thivarella <tl_varella@hotmail.com>
2017-08-23 23:09:53 -0300

    exercicio do dia 23 08 2017

---
+++ Exerc_dia_23-08-2017/Exercicio_Dia_23-08-2017/Exercicio_Dia_23-08-2017/Cachorro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Dia_23_08_2017
+{
+    class Cachorro
+    {
+        private string nome, raca;
+        private bool vacinado;
+
+        public Cachorro()
+        {
+
+        }
+        public Cachorro(string nome, string raca, bool vacinado)
+        {
+            this.Nome = nome;
+            this.Raca = raca;
+            this.Vacinado = vacinado;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + nome + " Raça: " + raca + " Vacinado: " + (vacinado ? "Sim" : "Não");
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Raca { get => raca; set => raca = value; }
+        public bool Vacinado { get => vacinado; set => vacinado = value; }
+    }
+}
---
+++ Exerc_dia_23-08-2017/Exercicio_Dia_23-08-2017/Exercicio_Dia_23-08-2017/Pessoa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Dia_23_08_2017
+{
+    class Pessoa
+    {
+        private string nome;
+        private long cpf;
+        private List<Cachorro> cachorros = new List<Cachorro>();
+        
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, long cpf)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+        }
+
+        public void AddCachorros(Cachorro c)
+        {
+            cachorros.Add(c);
+        }
+
+        public void RmCachorros(Cachorro c)
+        {
+            cachorros.Remove(c);
+        }
+
+        public void RmCachorros(int c)
+        {
+            cachorros.RemoveAt(c);
+        }
+
+        public Cachorro GetCachorro (int i)
+        {
+            return cachorros[i];
+        }
+
+        public int SizeOfCachorros()
+        {
+            return cachorros.Count;
+        }
+
+        public void ListAll()
+        {
+            foreach(Cachorro c in cachorros) {
+                Console.WriteLine(c);
+            }
+        }
+
+        public override string ToString()
+        {
+            String s = "CPF: " + cpf + " Nome: " + nome + " ";
+            foreach(Cachorro c in cachorros)
+                s += c + ", ";
+
+            return s;
+        }
+        public string Nome { get => nome; set => nome = value; }
+        public long Cpf { get => cpf; set => cpf = value; }
+    }
+}
